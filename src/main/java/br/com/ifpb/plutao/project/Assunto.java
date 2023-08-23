cf3f83d1b1cb000ab3b861c256b133e2855fed63
kilsonalvess <100282650+kilsonalvess@users.noreply.github.com>
2023-08-23 16:32:03 -0300

    Estrtuturas de Classes

--- src/main/java/br/com/ifpb/plutao/project/Assunto.java
+++ src/main/java/br/com/ifpb/plutao/project/Assunto.java
@@ -2,26 +2,5 @@ package br.com.ifpb.plutao.project;
 
 public class Assunto extends Aluno{
 
-    int id;
-    String nome;
 
-    @Override
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public String getNome() {
-        return nome;
-    }
-
-    @Override
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
